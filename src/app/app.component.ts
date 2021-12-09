f52d6cb89c2cb254df7caf743ca71f9014d62144
unknown <sherlinkuo@gmail.com>
2021-12-09 16:39:00 +0800

    基礎介紹

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  selector: 'app-root',  // 元件名稱
+  templateUrl: 'app.component.html', //合作的 HTML
+  styleUrls: ['app.component.scss'], //合作的 SCSS
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent { // Module 名稱
+  constructor() {} //建構函式(初始化的內容都放裡面)
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent], //運行的組件
+
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule], //專案用到的 Module
+
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],  //專案用到的服務
+  bootstrap: [AppComponent], //首頁
 })
 export class AppModule {}
