b0892de56e676d3c06b0ec8702cce513c6400786
unknown <sherlinkuo@gmail.com>
2021-12-09 18:07:41 +0800

    建立元件

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: 'app-home',  // 元件名稱
+  templateUrl: 'home.page.html', //合作的 HTML
+  styleUrls: ['home.page.scss'], //合作的 SCSS
 })
-export class HomePage {
+export class HomePage { // Module 名稱
 
-  constructor() {}
+  constructor() {}  //建構函式(初始化的內容都放裡面)
 
 }
+
