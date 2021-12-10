529bce1a258e3577acf2d5c7c120aa55a89ec93c
unknown <sherlinkuo@gmail.com>
2021-12-10 12:05:23 +0800

    HTML使用變數2

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -8,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class NewsComponent implements OnInit {
 
   title:string = "每日新聞";
+
+  newinfo = {
+    category:  "今日頭條",
+    title: "台灣9/27本土0確診, 陳時中: 10月5日起有條件開放部分休閒娛樂場所",
+    content: "指揮官陳時中今(27)日表示，考量國內疫情目前已趨緩且穩定控制，經與相關單位溝通討論及評估後，指揮中心規劃於本週起放寬部分場域防疫管制措施，並於10月5日起有條件開放部分休閒娛樂場所"
+
+
+  }
   constructor() { }
 
   ngOnInit() {}
